f4212e8a359bf80fe38bbb54bed151c7b6c523eb
Andy <andyhe04@gmail.com>
2021-08-23 10:46:17 +1000

    add all csv files and our result csv

---
+++ config.py
@@ -0,0 +1,4 @@
+result_folder = r"/home/ph/WFAResultRanking/202010_202105/"
+# result_folder = r"/home/ph/WFAResultRanking/test/"
+summary_columns = "Strategy,Market,HOReturn,HOSharpe,HOWinRate,HODrawRate,HOLoseRate,HOTotalTrades,BTReturn,BTMarketChange,BTWinRate,BTDrawRate,BTLoseRate,BTTotalTrades"
+ranked_folder = r"/home/ph/WFAResultRanking/rankings/"
