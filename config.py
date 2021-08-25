179d135a593e21e5914af1c66fdf785d351c54c1
AndyThatGuy <89520042+AndyThatGuy@users.noreply.github.com>
2021-08-25 21:28:58 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# This is a config
 result_folder = r"/home/ph/WFAResultRanking/202010_202105/"
 # result_folder = r"/home/ph/WFAResultRanking/test/"
 summary_columns = "Strategy,Market,HOReturn,HOSharpe,HOWinRate,HODrawRate,HOLoseRate,HOTotalTrades,BTReturn,BTMarketChange,BTWinRate,BTDrawRate,BTLoseRate,BTTotalTrades"
